6c0bcbd2d75e5d4e695ca8133308e34804faa91f
mujeebdev <mujeeb@psychx86.com>
2022-12-04 01:39:45 +0530

    has been build the todoList

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">Add</button>
+    </div>
+    <div class="tasks">
+      <!-- componnets -->
+      <!-- {{ $store.state.tasks }} -->
+      <!-- <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" /> -->
+      <Task :key="i" v-for="(task, i) in $store.state.tasks" :task="task" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.$store.commit("addTaskButton", this.newTask);
+        this.newTask = "";
+      }
+      // console.log(addTask);
+    },
+  },
+};
+</script>
---
+++ store/index.js
@@ -0,0 +1,17 @@
+export const state = () => ({
+  tasks: [],
+});
+
+export const mutations = {
+  addTaskButton(state, task) {
+    state.tasks = [{ content: task, done: false }, ...state.tasks];
+    // state.tasks.push({ content: task, done: false });
+    // state.tasks.unshift({ content: task, done: false });
+  },
+  removeTask(state, task) {
+    state.tasks.splice(state.tasks.indexOf(task), 1);
+  },
+  toggleTask(state, task) {
+    task.done = !task.done;
+  },
+};
